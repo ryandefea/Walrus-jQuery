89d73a55be68a455156b29bec2f7004e02f3d824
ryandefea <ryan.defea@gmail.com>
2022-06-01 12:18:15 -0700

    change method to slideToggle.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   $(".clickable").click(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
+  });
+});
+*/
+
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").slideToggle("50");
+    $("#walrus-hidden").slideToggle("50");
   });
 });
